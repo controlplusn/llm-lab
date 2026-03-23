0e6212ea5f5750bc1ab2ea1274fb2a850140ed2c
controlplusn <22-06230@g.batstate-u.edu.ph>
2026-03-23 23:01:44 +0800

    Add file path and input_ids in the dictionary

--- model/config.py
+++ model/config.py
@@ -1,11 +1,17 @@
 import json
+from pathlib import Path
 
+base_path = Path(__file__).parent
+print(base_path)
 
-def load_config(vocab_path="tokenizer/tokenizer/vocab.json"):
-    with open(vocab_path, "r") as f:
+vocab_path = base_path / "../tokenizer/tokenizer/vocab.json"
+
+def load_config(vocab_path=vocab_path):
+    with open(vocab_path, "r", encoding="utf-8") as f:
         vocab = json.load(f)
 
     return {
+        "input_ids": list(vocab.values()),
         "vocab_size": len(vocab),
         "d_model": 512,
         "num_heads": 8,
