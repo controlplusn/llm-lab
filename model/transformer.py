524d478d844c264ea22cdb17ce0a87252e7c293a
controlplusn <22-06230@g.batstate-u.edu.ph>
2026-03-23 23:02:48 +0800

    Model: Create embedding layer for the transformer architecture

---
+++ model/transformer.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from config import load_config
+
+CONFIG = load_config()
+
+# Token Embedding Layer
+class EmbeddingLayer:
+    def __init__(self, vocab_size, d_model):
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+
+        """
+            1. What I used - too small
+                - self.weights = np.random.randn(vocab_size, d_model) * 0.01 -> stats: mean: -0.0000, std: 0.0100
+            
+            2. Xavier / Glorot Initialization - general use, stable gradients:
+                - self.weights = np.random.randn(vocab_size, d_model) * np.sqrt(1.0 / d_model) -> stats: mean: -0.0000, std: 0.0442
+
+            3. Standard Normal - LLMs, emiprically proven:
+                - self.weights = np.random.normal(loc=0.0, scale=0.02, d_model=(vocab_size, d_model))
+        """
+        self.weights = np.random.randn(vocab_size, d_model) * np.sqrt(1.0 / d_model) 
+
+    def forward(self, token_ids):
+        return self.weights[token_ids]
+
+
+
+if __name__ == "__main__":
+    vocab = CONFIG["input_ids"]
+    vocab_size = CONFIG["vocab_size"]
+    d_model = CONFIG["d_model"]
+
+    print(f"Embedding layer size: {vocab_size * d_model}")
+
+    embedding_layer = EmbeddingLayer(vocab_size, d_model)
+    input_ids = np.array(vocab, dtype=int)
+    output_embedding = embedding_layer.forward(input_ids)
+
+
+    print(f"Input shape: {input_ids.shape}")
+    print(f"Output embeddings shape: {output_embedding.shape}")
+    print(f"Weight stats — mean: {embedding_layer.weights.mean():.4f}, std: {embedding_layer.weights.std():.4f}")
